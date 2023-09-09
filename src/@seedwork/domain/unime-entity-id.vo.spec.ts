9236797db76da3e72594855d897ff2f90c2f894d
Bruno Alves de Sousa <brunoalves@MacBook-Pro-de-Bruno.local>
2023-09-09 09:19:45 -0300

    create test uniqueentityId with spyon of jest

--- src/@seedwork/domain/unime-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unime-entity-id.vo.spec.ts
@@ -1,10 +1,30 @@
 import InvalidUuidError from "../../@seedwork/errors/invalid-uuid.error";
 import UniqueEntityId from "./unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+function spyValidateMethod() {
+  return jest.spyOn(UniqueEntityId.prototype as any, "validate");
+}
 
 describe("UniqueEntityId Unit Tests", () => {
   it("should throw error when uuid is invalid", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const validateSpy = spyValidateMethod();
     expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = spyValidateMethod();
+    const uuid = "014decd3-1781-47a5-bbef-effdb99f4d23";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = spyValidateMethod();
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.id)).toBeTruthy();
     expect(validateSpy).toHaveBeenCalled();
   });
 });
